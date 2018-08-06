348873f614a880247135df547c9695707793811f
caleblareau <caleblareau@g.harvard.edu>
2018-08-06 02:12:01 -0400

    kmer analysis

--- code/02_annotationProportions.R
+++ code/02_annotationProportions.R
@@ -14,24 +14,40 @@ x02 <- bedToGRanges("../annotations/UCSC_Exons.bed")
 x03 <- bedToGRanges("../annotations/UCSC_5primeUTR.bed")
 x04 <- bedToGRanges("../annotations/UCSC_Introns.bed")
 
-# Take a sample and 
-gr_t <- addchr(makeGRangesFromDataFrame(read.table("../output/LIN28B-Rep1.bed", header = TRUE)))
-seq_t <- getSeq(BSgenome.Hsapiens.UCSC.hg19, gr_t)
-motif <- as.numeric((vcountPattern("GGAGA", seq_t) + vcountPattern("TCTCC", seq_t)) > 0)
+# Take a sample and run with it
+getProportions <- function(file, sample){
+  gr_t <- addchr(makeGRangesFromDataFrame(read.table(file, header = TRUE)))
+  seq_t <- getSeq(BSgenome.Hsapiens.UCSC.hg19, gr_t)
 
-# Do overlaps
-ov_1 <- findOverlaps(gr_t, x01)
-ov_2 <- findOverlaps(gr_t, x02)
-ov_3 <- findOverlaps(gr_t, x03)
-ov_4 <- findOverlaps(gr_t, x04)
+  # Do overlaps
+  ov_1 <- findOverlaps(gr_t, x01)
+  ov_2 <- findOverlaps(gr_t, x02)
+  ov_3 <- findOverlaps(gr_t, x03)
+  ov_4 <- findOverlaps(gr_t, x04)
+  
+  # Classify each variant
+  class <- ifelse(1:length(gr_t) %in% queryHits(ov_1), "3UTR",
+                  ifelse(1:length(gr_t) %in% queryHits(ov_2), "Exon",
+                         ifelse(1:length(gr_t) %in% queryHits(ov_3), "5UTR",
+                                ifelse(1:length(gr_t) %in% queryHits(ov_4), "Intron", "other"))))
+  
+  data.frame(class) %>%
+    group_by(class) %>% 
+    summarise (n = n()) %>%
+    filter(class != "other") %>%
+    mutate(freq = n / sum(n)) %>%
+    reshape2::melt() %>% filter(variable == "freq") %>%
+    mutate(sample = sample)
+}
 
-# Classify each variant
-class <- ifelse(1:length(gr_t) %in% queryHits(ov_1), "3UTR",
-                ifelse(1:length(gr_t) %in% queryHits(ov_2), "Exon",
-                       ifelse(1:length(gr_t) %in% queryHits(ov_3), "5UTR",
-                              ifelse(1:length(gr_t) %in% queryHits(ov_4), "Intron", "other"))))
+propdf <- rbind(getProportions("../output/LIN28B-IDR.bed", "Consensus"),
+      getProportions("../output/LIN28B-Rep1.bed", "Rep1"),
+      getProportions("../output/LIN28B-Rep2.bed", "Rep2"))
 
-data.frame(class, motif) %>%
-  group_by(motif, class) %>% 
-  summarise (n = n()) %>%
-  mutate(freq = n / sum(n))
+easter_colors <- c("3UTR" =	"#e0cdff", "Exon" = "#dcf9a8", "Intron" = "#c1f0fb")
+ggplot(propdf, aes(x = sample, y = value*100, fill = class)) + 
+  geom_bar(stat = "identity", color = "black") +
+  scale_fill_manual(values = c( "gray30", "gray40", "lightgrey")) +
+  labs(x = "", y = "% of peak annotations", fill = "")  + pretty_plot(fontsize = 8) +
+  L_border() -> propPlot
+cowplot::ggsave(propPlot, file = "../output/plots/proportion_plot.pdf", width = 2.2, height = 2)
--- code/03_5mer_analysis.R
+++ code/03_5mer_analysis.R
@@ -10,29 +10,47 @@ library(SummarizedExperiment)
 
 "%ni%" <- Negate("%in%")
 
-peaks <- bedToGRanges("../consensus/lin28b_consensus.bed")
-counts <- data.matrix(read.table("../consensus/chromVAR_LIN28B.counts.tsv", header = TRUE))
+# Annotations
+x01 <- bedToGRanges("../annotations/UCSC_3primeUTR.bed")
+x02 <- bedToGRanges("../annotations/UCSC_Exons.bed")
+x03 <- bedToGRanges("../annotations/UCSC_5primeUTR.bed")
+x04 <- bedToGRanges("../annotations/UCSC_Introns.bed")
+
+peaks <- addchr(makeGRangesFromDataFrame(read.table("../output/LIN28B-IDR.bed",
+                                                    header = TRUE), keep.extra.columns = TRUE))
+#peaks<- resize(peaks, width = 60, fix="center")
 
 SE <- SummarizedExperiment::SummarizedExperiment(
   rowRanges = peaks, 
-  colData = data.frame(samples = c("LIN28B", "PURA")), 
-  assays = list(counts = cbind(rowSums(counts[,c(1,2)]), rowSums(counts[,c(3,4)]))))
-
-if(FALSE){
-  SE <- SummarizedExperiment::SummarizedExperiment(
-    rowRanges = peaks, 
-    colData = data.frame(samples = colnames(counts)), 
-    assays = list(counts = counts))
-}
+  colData = data.frame(samples = ""), 
+  assays = list())
 
 # Compute deviation scores for 5-mers
-SE <- filterPeaks(SE)
-kmer_ix <- matchKmers(5, SE, genome = BSgenome.Hsapiens.UCSC.hg19)
-SE <- addGCBias(SE, genome = BSgenome.Hsapiens.UCSC.hg19)
-dev <- computeDeviations(object = SE, annotations = kmer_ix)
+kmer_ix <- matchKmers(4, SE, genome = BSgenome.Hsapiens.UCSC.hg19)
+ov_1 <- findOverlaps(peaks, x01)
+ov_2 <- findOverlaps(peaks, x02)
+ov_3 <- findOverlaps(peaks, x03)
+ov_4 <- findOverlaps(peaks, x04)
 
-df_out <- data.frame(Rep1 = assays(dev)[["z"]][,1],
-                     Rep2 = assays(dev)[["z"]][,2], 
-                     fiveMer = names(assays(dev)[["z"]][,2]))
+class <- ifelse(1:length(peaks) %in% queryHits(ov_1), "3UTR",
+                ifelse(1:length(peaks) %in% queryHits(ov_2), "Exon",
+                       ifelse(1:length(peaks) %in% queryHits(ov_3), "5UTR",
+                              ifelse(1:length(peaks) %in% queryHits(ov_4), "Intron", "other"))))
 
-df_out %>% arrange(desc(Rep1)) %>% filter(fiveMer == "TCTCC")
+dff <- data.frame(class, data.matrix(assays(kmer_ix)[["matches"]])*1)
+
+dff %>% group_by(class) %>%  summarise_all(funs(mean)) %>%
+  dplyr::select(-one_of("class")) %>% data.frame() %>% t() -> propDF
+
+propDF["CCTC",]
+propDF["CTCC",]
+plot_df <- data.frame(x = propDF[,1]*100, y = propDF[,2]*100, motif = rownames(propDF))
+plot_df$color <- plot_df$x > 60 & plot_df$y > 60
+
+p1 <- ggplot(plot_df, aes(x = x, y = y, color = color)) + geom_point(size = 0.2) +
+  pretty_plot(fontsize = 8) +
+  L_border() + labs(x = "% of Peaks - 3'UTR", y = "% of Peaks - Coding Exon") +
+  scale_color_manual(values = c("lightgrey", "black")) + theme(legend.position = "none") +
+  geom_hline(yintercept = 60, linetype = 2) + geom_vline(xintercept = 60,  linetype = 2)
+
+cowplot::ggsave(p1, file = "../output/plots/4mer.pdf", width = 2, height = 2)
